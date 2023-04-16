2a48faeba447751db375643b6d6f52ef7affba58
ElizavetaSmirnova <108726784+ElizavetaSmirnova@users.noreply.github.com>
2023-04-17 00:17:29 +0500

    started of main menu

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,8 @@ let navbar = document.querySelector('.navbar')
 window.onscroll = () => {
     searchBtn.classList.remove('fa-times')
     searchBar.classList.remove('active')
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
 }
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times')
